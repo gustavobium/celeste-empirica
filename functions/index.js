d5b256aa3ef8f61655b5afff7b1ee45721da4571
gustavobium <37386964+gustavobium@users.noreply.github.com>
2019-11-25 22:43:43 -0300

    paginas completas

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
+const firebaseui = require('firebaseui');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
